2df5423d862fe06ea61b99cfef1458c83a091948
mdalaminali <alaminmmc5@gmail.com>
2023-02-03 00:39:23 +0600

    slice and splice

---
+++ slice.js
@@ -0,0 +1,7 @@
+const numbers =[12,233,34,45,34,553,534,32,12,34,553,]
+
+console.log(numbers)
+
+
+const slice = numbers.slice(3,7);
+console.log(slice);
---
+++ splice.js
@@ -0,0 +1,12 @@
+const numbers =[12,233,34,45,34,553,534,32,12,34,553,]
+
+console.log(numbers)
+
+// const splice = numbers.splice(4,7);
+// console.log( splice);
+
+
+
+
+const splice = numbers.splice(4,7, 66, 777, 9999);
+console.log( splice);
