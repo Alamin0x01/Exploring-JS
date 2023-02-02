7e121a62bf19a2ea60a061ca2fc57ff1bd044598
mdalaminali <alaminmmc5@gmail.com>
2023-02-02 23:55:56 +0600

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+const country = 'bangladesh';
+const age ='54';
+const friends = [23,32, 23, 23,4, 4, 42, 24,];
+
+
+
+
+console.log(typeof country);
+
+console.log(typeof age);
+
+
+console.log(Array.isArray(friends));
--- case.js
+++ case.js
@@ -1,8 +1,14 @@
-const userName = "blackPink";
-const userInput = "blackPinK";
-console.log(userName.toLowerCase);
-if (userName.toLowerCase === userInput.toLowerCase) {
-  console.log("valid user");
-} else {
-  console.log("invalid user");
+// const userName = "blackPink";
+// const userInput = "blackPinK";
+// console.log(userName.toLowerCase);
+// if (userName.toLowerCase === userInput.toLowerCase) {
+//   console.log("valid user");
+// } else {
+//   console.log("invalid user");
+// }
+
+
+function add(a, b){
+  return a + b;
 }
+console.log(add("adam" + "eve"))
