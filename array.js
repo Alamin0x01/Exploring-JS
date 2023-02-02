84487295303266869c5031a1ec1a47140a3a6f15
mdalaminali <alaminmmc5@gmail.com>
2023-02-03 00:15:49 +0600

    concat

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 const country = 'bangladesh';
 const age ='54';
-const friends = [23,32, 23, 23,4, 4, 42, 24,];
+const FriendsAge = [23,32, 23, 23,4, 4, 42, 24,];
 
 
 
@@ -9,8 +9,19 @@ console.log(typeof country);
 
 console.log(typeof age);
 
-console.log(friends.includes(6));
-console.log(friends.includes(42));
+console.log(FriendsAge.includes(6));
+console.log(FriendsAge.includes(42));
+
+// if (friendsAge.indexOf(55)  !== -1 ) {
+    
+// }
 
 
-console.log(Array.isArray(friends));
+console.log(Array.isArray(FriendsAge));
+
+
+
+// concat
+const newFriendsAge = [23,33, 233, 5,34,53,23, 55,223,];
+const  allFriendsAge = newFriendsAge.concat(FriendsAge);
+console.log(allFriendsAge);
