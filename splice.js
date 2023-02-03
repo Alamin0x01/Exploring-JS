d491d0fcc77f21000c7aa61c9176ec6e80316a21
mdalaminali <alaminmmc5@gmail.com>
2023-02-03 17:57:17 +0600

    sum add

--- splice.js
+++ splice.js
@@ -2,11 +2,26 @@ const numbers =[12,233,34,45,34,553,534,32,12,34,553,]
 
 console.log(numbers)
 
-// const splice = numbers.splice(4,7);
+const splice = numbers.splice(4,7);
+console.log( splice);
+
+
+
+// const splice = numbers.splice(4,7, 66, 777, 9999);
 // console.log( splice);
 
 
 
 
-const splice = numbers.splice(4,7, 66, 777, 9999);
-console.log( splice);
+
+
+
+
+
+
+
+// let sum=0; 
+// for( let i = 0; i<=3;i++){ 
+// sum = sum + i; 
+// }
+// console.log(sum);
